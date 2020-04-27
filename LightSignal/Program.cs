f7fa791f10228ad43d28e0bbb9329f3392eb29ad
Rinat <bloomder@mail.ru>
2020-04-27 16:48:48 +0300

    Добавьте файлы проекта.

---
+++ LightSignal/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LightSignal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SignalLigth sl = new SignalLigth();
+            bool a = true;
+            int ch = 0;
+            string str="";
+            while(a)
+            {
+                Console.Clear();
+                str = "";
+                if (sl.color!=null)
+                {
+                    Console.WriteLine("Светофор светиться: " + sl.color.ToString());
+                }
+                Console.WriteLine("Выберите значение, чтобы указать цвет светофора:");
+                Console.WriteLine("1. Green\r\n2. Yellow\r\n3. Red");
+                try
+                {
+                    str += Console.ReadKey().KeyChar;
+                    ch = Convert.ToInt32(str);
+                }
+                catch(Exception ex)
+                {
+                    if(str=="q")
+                    {
+                        a = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ввели не допустимое значение!");
+                        Thread.Sleep(1000);
+                    }
+                }
+                sl.ChangeColor(ch);
+            }
+        }
+    }
+}
---
+++ LightSignal/SignalLigth.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightSignal
+{
+    class SignalLigth
+    {
+        public string color;
+        public virtual void ChangeColor(int i) // Я могу ошибиться, но виртуал нужен для 
+        {// переопределния, у дорожного 4 значения, у пешего 3
+            switch(i)
+            {
+                case 0:
+                    {
+                        color = null;
+                        break;
+                    }
+                case 1:
+                    {
+                        color = "Green";
+                        break;
+                    }
+                case 2:
+                    {
+                        color = "Yellow";
+                        break;
+                    }
+                case 3:
+                    {
+                        color = "Red";
+                        break;
+                    }
+            }
+        }
+    }
+}
