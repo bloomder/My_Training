33269141aed6e1f14ab1f7175ca195e0694d144a
Rinat <bloomder@mail.ru>
2020-04-28 11:25:31 +0300

    Нельзя переключиться с красного цвета на зеленный и наоборот.

---
+++ LightSignal1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LightSignal1
+{
+    class Program
+    {
+        static bool flag_while = true;
+        static Svetofor svetofor;
+        static string stroka = "";
+        static bool flag_error = false;
+        static void Main(string[] args)
+        {
+            svetofor = new Svetofor(4); 
+            while(flag_while)
+            {
+                Console.Clear();
+                if (flag_error) Console.WriteLine("Допущена ошибка! Повторите действие.");
+                Console.WriteLine(svetofor.StateLightColor());
+                Console.Write("Выберите действие:");
+                stroka = "";
+                for (int i = 0; i < svetofor.Property_light_signal.Mass_colors.Length/2; i++)
+                {
+                    if (i == 0) stroka += "\r\n1. Выключить светофор.";
+                    else
+                    {
+                        stroka += $"\r\n{(i+1).ToString()}. Зажечь {svetofor.Property_light_signal.Mass_colors[i, 0].ToLower()} цвет на светофоре.";
+                    }
+                }
+                stroka += "\r\nq. Выйти из программы.";
+                Console.WriteLine(stroka); stroka = "";
+                try
+                {
+                    stroka += Console.ReadKey().KeyChar;
+                    if (stroka == "q") { flag_while = false; stroka = ""; }
+                    else
+                    {
+                        if (svetofor.ChangeLightColor((Convert.ToInt32(stroka) - 1))) { flag_error = false; }
+                        else flag_error = true;
+                        stroka = "";
+                    }
+                }
+                catch(Exception ex)
+                {
+                    stroka = ""; flag_error = true;
+                }
+            }
+        }
+    }
+}
---
+++ LightSignal1/PropertyLightSignal.cs
@@ -0,0 +1,31 @@
+﻿namespace LightSignal1
+{
+    class PropertyLightSignal
+    {
+        public int Numb_color { get; set; }
+        int numb_color;
+        public string[,] Mass_colors { get { return mass_colors; } }
+        string[,] mass_colors;
+        public PropertyLightSignal()
+        {
+            numb_color = 0;
+            AddMassColors(mass_colors);
+        }
+        protected virtual void AddMassColors(string[,] mass_colors_met)
+        {
+            mass_colors_met = new string[4, 2];
+            mass_colors_met[0, 0] = null;
+            mass_colors_met[0, 1] = null;
+
+            mass_colors_met[1, 0] = "Зелёный";
+            mass_colors_met[1, 1] = "Зелёным";
+
+            mass_colors_met[2, 0] = "Жёлтый";
+            mass_colors_met[2, 1] = "Жёлтым";
+
+            mass_colors_met[3, 0] = "Красный";
+            mass_colors_met[3, 1] = "Красным";
+            mass_colors = mass_colors_met;
+        }
+    }
+}
