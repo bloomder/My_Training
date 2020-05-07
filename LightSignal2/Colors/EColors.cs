0ee9bf6da8851ef7fc3a77db2bbd0625ad48dd5e
Rinat <bloomder@mail.ru>
2020-05-07 14:19:02 +0300

    Оператор, c помощью Enter переключает все цвета(Зеленый, Желтый, Красный). Но не может переключить сразу с Зеленого на Красный и наоборот.

---
+++ LightSignal2/Colors/EColors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightSignal2.Colors
+{
+    public enum EColors
+    {
+        Red, 
+        Yellow, 
+        Green
+    }
+}
---
+++ LightSignal2/TrafficControl.cs
@@ -0,0 +1,43 @@
+﻿using LightSignal2.Colors;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightSignal2
+{
+    class TrafficControl : ITrafficControl
+    {
+        EColors eColors;
+        bool SwitchDown =true;
+        public TrafficControl(EColors eColors)
+        {
+            this.eColors = eColors;
+        }
+
+        public void SwitchLight()
+        {
+            switch (eColors)
+            {
+                case EColors.Red:
+                    eColors = EColors.Yellow;
+                    break;
+                case EColors.Yellow:
+                    eColors = SwitchDown == true ? EColors.Red : EColors.Green;
+                    SwitchDown = SwitchDown == true ? false : true;
+                    break;
+                case EColors.Green:
+                    eColors = EColors.Yellow;
+                    break;
+            }
+        }
+
+        public string StateColor()
+        {
+            return (string)($"Светофор горит: {eColors.ToString()}");
+        }
+    }
+}
