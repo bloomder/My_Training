8fbd98b6d40b0e75b41c71deed83770373c5eab8
Administrator <Administrator@DESKTOP-7GHCN6I>
2020-09-23 18:32:25 +0300

    add

---
+++ Shop2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop2
+{
+    //Сделать магазин. Есть список товаров и цена на них. Нужно считать на какую сумму покупатель набрал товара.
+
+    class Shop
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
